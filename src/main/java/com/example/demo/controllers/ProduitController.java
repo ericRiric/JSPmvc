8bb04527ad1ffd3923f79ad2040b10ec8f10eb95
ericRiric <2467672@cegepoutaouais.qc.ca>
2026-08-26 09:53:12 -0400

    Ajouter les données à l'affichage

--- src/main/java/com/example/demo/controllers/ProduitController.java
+++ src/main/java/com/example/demo/controllers/ProduitController.java
@@ -2,20 +2,22 @@ package com.example.demo.controllers;
 
 import com.example.demo.services.ProduitService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/produit")
 public class ProduitController {
-    private ProduitService produitService;
+    private final ProduitService produitService;
 
     public ProduitController() {
         produitService = new ProduitService();
     }
 
     @GetMapping("/index")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("produits", produitService.getProduitArrayList());
         return "produit";
     }
 }
