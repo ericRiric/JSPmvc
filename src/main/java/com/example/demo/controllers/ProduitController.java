b1f333c36186aa95752ff936a4f8b4ba0304c5f7
ericRiric <2467672@cegepoutaouais.qc.ca>
2026-08-26 09:40:21 -0400

    Enlever servlet inutilisé

--- src/main/java/com/example/demo/controllers/ProduitController.java
+++ src/main/java/com/example/demo/controllers/ProduitController.java
@@ -1,5 +1,8 @@
 package com.example.demo.controllers;
 
+import com.example.demo.models.Produit;
+import com.example.demo.repository.IProduitRepository;
+import com.example.demo.repository.ProduitRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +10,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/produit")
 public class ProduitController {
+    private IProduitRepository produitRepository;
+
+    public ProduitController() {
+        produitRepository = new ProduitRepository();
+
+        produitRepository.ajouterProduit(new Produit(
+                0,
+                "Goonoid",
+                10.99,
+                5,
+                false
+        ));
+        produitRepository.ajouterProduit(new Produit(
+                1,
+                "Goon",
+                20.99,
+                12,
+                true
+        ));
+    }
+
     @GetMapping("/index")
     public String index() {
         return "produit";
