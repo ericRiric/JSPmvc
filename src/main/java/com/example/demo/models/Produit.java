abeb54aec5c779a81136313cacad0e4684cf898d
ericRiric <2467672@cegepoutaouais.qc.ca>
2026-09-08 10:51:17 -0400

    Fix fonction getFirst > get(0)

--- src/main/java/com/example/demo/models/Produit.java
+++ src/main/java/com/example/demo/models/Produit.java
@@ -7,17 +7,26 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "jsp_db")
+@Table(name = "produits")
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Builder
 public class Produit {
     @Id
+    @Column(name = "no_produit")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer noProduit;
+
+    @Column(name = "nom")
     private String nom;
+
+    @Column(name = "prix")
     private Double prix;
+
+    @Column(name = "quantite")
     private Integer quantite;
+
+    @Column(name = "taxable")
     private Boolean taxable;
 }
