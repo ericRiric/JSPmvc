bd9e2f2925776770a11ae0d76efccedff6c1acab
ericRiric <2467672@cegepoutaouais.qc.ca>
2026-09-08 10:25:31 -0400

    Ajout database à moitié

--- src/main/java/com/example/demo/models/Produit.java
+++ src/main/java/com/example/demo/models/Produit.java
@@ -1,57 +1,23 @@
 package com.example.demo.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "jsp_db")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
 public class Produit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer noProduit;
     private String nom;
     private Double prix;
     private Integer quantite;
     private Boolean taxable;
-
-    public Produit(Integer noProduit, String nom, Double prix, Integer quantite, Boolean taxable) {
-        this.noProduit = noProduit;
-        this.nom = nom;
-        this.prix = prix;
-        this.quantite = quantite;
-        this.taxable = taxable;
-    }
-
-    public Integer getNoProduit() {
-        return noProduit;
-    }
-
-    public void setNoProduit(Integer noProduit) {
-        this.noProduit = noProduit;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public Double getPrix() {
-        return prix;
-    }
-
-    public void setPrix(Double prix) {
-        this.prix = prix;
-    }
-
-    public Integer getQuantite() {
-        return quantite;
-    }
-
-    public void setQuantite(Integer quantite) {
-        this.quantite = quantite;
-    }
-
-    public Boolean getTaxable() {
-        return taxable;
-    }
-
-    public void setTaxable(Boolean taxable) {
-        this.taxable = taxable;
-    }
 }
--- src/main/java/com/example/demo/repository/IProduitRepository.java
+++ src/main/java/com/example/demo/repository/IProduitRepository.java
@@ -1,10 +1,11 @@
 package com.example.demo.repository;
 
 import com.example.demo.models.Produit;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.ArrayList;
 
-public interface IProduitRepository {
+public interface IProduitRepository extends JpaRepository<Produit, Integer> {
     ArrayList<Produit> getProduitArrayList();
     Produit getProduitItem(Integer id);
     void ajouterProduit(Produit produit);
